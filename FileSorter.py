a3243b7fcf42e44372649c93c1420e4f247d1372
LarryWons <76619701+LarryWonss@users.noreply.github.com>
2022-02-13 11:53:24 +0800

    Update FileSorter.py

--- FileSorter.py
+++ FileSorter.py
@@ -1,9 +1,13 @@
+'''
+脚本功能：按照文件后缀名分门别类整理文件夹，并将同类型的文件移动到同个子文件夹中。
+'''
+
 import os
 import shutil
-src_folder = input('输入你想整理的文件夹的绝对路径：')   #'/Users/jackeroo/Downloads/'
-des_folder = input('输入整理后文件放置的文件夹绝对位置：')  #'/Users/jackeroo/Downloads/sorted/'
+src_folder = input('Type the source fold:\n输入你想整理的文件夹的绝对路径：')   
+des_folder = input('Type the destination fold:\n输入整理后文件放置的文件夹绝对位置：')  
 files = os.listdir(src_folder)
-print('文件整理中...')
+print('Files sorting now...\n文件整理中...')
 for file in files:
     src_path = src_folder + file
     if os.path.isfile(src_path):
@@ -11,4 +15,4 @@ for file in files:
         if not os.path.exists(des_path):
             os.makedirs(des_path)
         shutil.move(src_path,des_path)
-print('文件整理完毕！')
+print('Done!\n文件整理完毕！')
